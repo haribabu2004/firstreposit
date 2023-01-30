e87582f402e46d7da284f7634802bd0a6a0c26e1
haribabu2004 <hariarun2004@gmail.com>
2023-01-31 00:13:11 +0530

    changed a = 200 in main branch

--- sub.c
+++ sub.c
@@ -3,7 +3,7 @@ int main(){
 
     // int a=5;  // removed the extra codes
     // int a=32;
-    int a=20; //i don't use 5 and 32 instead used a=20.
+    int a=200; //i don't use 5 and 32 instead used a=20.
     int b=10;
     printf(a-b);
 }
